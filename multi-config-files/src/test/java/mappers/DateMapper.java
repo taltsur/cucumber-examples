f6f2dbb3549edb7efd35b13bd66f81ba5ee9feff
Ida Amit <Ida.Amit@nice.com>
2018-08-20 10:58:18 +0300

    Added DateMapper class

---
+++ multi-config-files/src/test/java/mappers/DateMapper.java
@@ -0,0 +1,19 @@
+package mappers;
+
+import cucumber.api.Transformer;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateMapper extends Transformer<LocalDate> {
+
+    @Override
+    public LocalDate transform(String date) {
+
+        //Not too sure about the date pattern though, check it out if it gives correct result
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return LocalDate.parse(date, formatter);
+    }
+
+}
